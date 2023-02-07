54ff6aa847d5f400314f5dc791c08b27b5c15a4c
JACKFIRE911 <109713399+JACKFIRE911@users.noreply.github.com>
2023-02-07 18:01:45 +0530

    Update them.sh

--- them.sh
+++ them.sh
@@ -1,3 +1,15 @@
-cp Logo.txt /data/data/com.termux/files/usr/etc
+
+
+cp logo.txt /data/data/com.termux/files/usr/etc
+
 rm /data/data/com.termux/files/usr/etc/bash.bashrc
+
 cp bash.bashrc /data/data/com.termux/files/usr/etc
+
+rm /data/data/com.termux/files/usr/etc/motd.sh
+
+touch /data/data/com.termux/files/usr/etc/motd.sh
+
+rm /data/data/com.termux/files/usr/etc/motd
+
+touch /data/data/com.termux/files/usr/etc/motd
